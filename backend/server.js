45021aa83823920646b1cacdda0047bd16dacf36
rocketwolf98 <juanico.hernel064@gmail.com>
2025-02-24 11:07:40 +0800

    CI: Implemented db Configuration

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
+import { connect } from "mongoose";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+app.get("/library", (req, res) => {
     res.send("Service is Running but Updated");
 });
 
 app.listen(PORT, () => {
+    connectDB();
     console.log("Server stated at http://localhost:" + PORT);
 });
