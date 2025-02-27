bdd48cf7427e47b3b089f2a83519e53141b1fe01
iragca <irag.chrisandrei@mailbox.org>
2025-02-27 17:11:32 +0800

    refactor: tested db connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+
+// Import routes
 import index from './routes/index.js';
 import fallback from './routes/fallback.js';
+import books from './routes/books.js';
 
 const app = express();
 
@@ -13,6 +17,8 @@ app.use('/', index);
 
 app.post("/postbook", (req, res) => {});
 
+app.use('/books', books);
+
 // 404 Fallback route - Add this before error handler but after all other routes
 app.use('*', fallback);
 
@@ -21,5 +27,14 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
+
+// Connect to DB
+try {
+  connectDB();
+} catch (error) {
+  console.error(`No DB connection: ${error.message}`);
+  process.exit(1);
+}
+
 
 export default app;
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,34 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function run() {
+  const clientOptions = {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+    useNewUrlParser: true,
+  };
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      `🚀 Pinged your deployment. Successfully connected!
+      Host: ${mongoose.connection.host}
+      Collection: ${mongoose.connection.name}
+      URI: ${process.env.MONGO_URI}`
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error){
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    run().catch(console.dir);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
