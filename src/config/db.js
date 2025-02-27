787f576420ec1939340c2dd225cf3a94255caa79
iragca <irag.chrisandrei@mailbox.org>
2025-02-27 18:05:02 +0800

    refactor: added some try catches and log messages

--- src/config/db.js
+++ src/config/db.js
@@ -12,12 +12,26 @@ async function run() {
 
     await mongoose.connect(process.env.MONGO_URI, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
+
+    console.log("🟩 MongoDB connected successfully.");
     console.log(
-      `🚀 Pinged your deployment. Successfully connected!
+      `🚀 Pinged your deployment.
       Host: ${mongoose.connection.host}
       Collection: ${mongoose.connection.name}
       URI: ${process.env.MONGO_URI}`
     );
+  } catch (error) {
+    if (error instanceof mongoose.Error) {
+      console.error(`MongooseError: ${error.message}`);
+      console.log(
+        "🟨 TIP: Ensure that your .env file is set up correctly with the MONGO_URI key."
+      );
+    } else {
+      console.error(`Error: ${error}`);
+    }
+    await mongoose.disconnect();
+    console.log("🟥 MongoDB connection failed. Shutting down server.");
+    process.exit(1);
   } finally {
     // Ensures that the client will close when you finish/error
     await mongoose.disconnect();
